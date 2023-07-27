7b85e38ecf7d47b2558d5d9efbbd02c10c40e8c8
Jackson085 <jackson26594916@gmail.com>
2023-07-27 18:00:35 +0200

    add collectionConnector

--- src/DatabaseConnector/DatabaseConnector.py
+++ src/DatabaseConnector/DatabaseConnector.py
@@ -2,7 +2,34 @@ import pymongo
 
 
 class DatabaseConnector:
-    def __init__(self, database_name: str, collection_name: str):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = object.__new__(cls)
+        return cls._instance
+
+    def __init__(self, database_name: str):
         self.db_client = pymongo.MongoClient()
         self.mydb = self.db_client[database_name]
-        self.collection = self.mydb[collection_name]
+
+
+class CollectionConnector:
+    def __init__(self, database_name, collection_name: str):
+        self.db_connector = DatabaseConnector(database_name)
+        self.collection = self.db_connector.mydb[collection_name]
+
+
+if __name__ == '__main__':
+    c = CollectionConnector('testDB', 'collection1')
+    c2 = CollectionConnector('testDB', 'collection2')
+
+    print(c.collection)
+    print(c2.collection)
+    print(c is c2)
+
+    c.collection.insert_one({'test': 1})
+    c2.collection.insert_one({'test': 1})
+
+    print(c.db_connector.mydb)
+    print(c2.db_connector.mydb)
