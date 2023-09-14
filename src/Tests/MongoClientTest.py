6b299f727268d5129156625ff9de6f1b5d4b6414
Jackson085 <jackson26594916@gmail.com>
2023-09-14 12:17:03 +0200

    Create MongoClientTest.py

---
+++ src/Tests/MongoClientTest.py
@@ -0,0 +1,22 @@
+import unittest
+from DatabaseConnector.MongoClient import MongoClient
+
+
+class MyTestCase(unittest.TestCase):
+    def test_client_instances(self):
+        ins1 = MongoClient('DatabaseTest', 'CollectionTest1').collection
+        ins2 = MongoClient('DatabaseTest', 'CollectionTest2').collection
+        ins3 = MongoClient('DatabaseTest', 'CollectionTest2').collection
+        ins4 = MongoClient('DatabaseTest1', 'CollectionTest').collection
+
+        self.assertEqual(ins2, ins3)
+        self.assertNotEqual(ins1, ins2)
+        self.assertNotEqual(ins1, ins3)
+        self.assertNotEqual(ins1, ins4)
+        self.assertNotEqual(ins3, ins4)
+
+    # no more test (for now) because MongoClient has same functions as pymongo.MongoClient
+
+
+if __name__ == '__main__':
+    unittest.main()
