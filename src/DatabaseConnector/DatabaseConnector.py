c608409752e376df65b298dc4c6e9de6785c6ee9
Jackson085 <jackson26594916@gmail.com>
2023-09-14 12:04:00 +0200

    clean

--- src/DatabaseConnector/DatabaseConnector.py
+++ src/DatabaseConnector/DatabaseConnector.py
@@ -17,32 +17,3 @@ class DatabaseConnector:
         self.db_client = pymongo.MongoClient()
         self.mydb = self.db_client[database_name]
         self.collection = self.mydb[collection_name]
-
-
-# class CollectionConnector:
-#     def __init__(self, database_name, collection_name: str):
-#         self.db_connector = DatabaseConnector(database_name)
-#         self.collection = self.db_connector.mydb[collection_name]
-
-
-if __name__ == '__main__':
-    c = DatabaseConnector('test', 'collection')
-    c1 = DatabaseConnector('test2', 'collection2')
-    c2 = DatabaseConnector('test2', 'collection2')
-    c3 = DatabaseConnector('test2', 'collection')
-    print(c is c2)
-    print(c is c1)
-    print(c1 is c2)
-    print(c is c3)
-    # c = CollectionConnector('testDB', 'collection1')
-    # c2 = CollectionConnector('testDB', 'collection1')
-    #
-    # print(c.collection)
-    # print(c2.collection)
-    # print(c is c2)
-    #
-    # c.collection.insert_one({'test': 1})
-    # c2.collection.insert_one({'test': 1})
-    #
-    # print(c.db_connector.mydb)
-    # print(c2.db_connector.mydb)
