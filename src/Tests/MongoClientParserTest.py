ce2f8e2053b242cff5c9648d9907088809993b4b
Jackson085 <jackson26594916@gmail.com>
2023-10-10 20:15:42 +0200

    add test

--- src/Tests/MongoClientParserTest.py
+++ src/Tests/MongoClientParserTest.py
@@ -47,12 +47,12 @@ class MongoClientParserTest(unittest.TestCase):
     def test_object_with_2dlist_of_object_to_dict(self):
         cls = ClassWithObjectsIn2DList()
 
-        self.assertEqual({'parent_var': 1, 'my_list': [{'SimpleClass': {'var1': 1, 'var2': 2}}, {'SimpleClass': [{'SimpleClass': {'var1': 1, 'var2': 2}}]}]},  self.ins._parse_object_to_dict(cls))
+        self.assertEqual({'my_list': [{'SimpleClass': {'var1': 1, 'var2': 2}}, [{'SimpleClass': {'var1': 1, 'var2': 2}}]], 'parent_var': 1},  self.ins._parse_object_to_dict(cls))
 
     # region parse json to class
     def test_json_to_class(self):
         json = {'var1': 1, 'var2': 2}
-        loaded_class = self.ins._parse_result_to_object(json, SimpleClass)
+        loaded_class = self.ins._parse_dict_to_object(json, SimpleClass)
 
         # self.assertIsInstance(loaded_class, SimpleClass)
         self.assertEqual(1, loaded_class.var1)
@@ -61,7 +61,7 @@ class MongoClientParserTest(unittest.TestCase):
     def test_dict_with_subclass_to_object(self):
         json = {'normal_var': 1, 'sub_class': {'var1': 1, 'var2': 2}}
 
-        loaded_class = self.ins._parse_result_to_object(json, ClassWithSubclass, {'sub_class': SimpleClass})
+        loaded_class = self.ins._parse_dict_to_object(json, ClassWithSubclass, {'sub_class': SimpleClass})
 
         # self.assertIsInstance(loaded_class, ClassWithSubclass)
         self.assertEqual(1, loaded_class.normal_var)
@@ -72,7 +72,8 @@ class MongoClientParserTest(unittest.TestCase):
     def test_dict_with_two_subclass_to_object(self):
         json = {'child_class': {'normal_var': 1, 'sub_class': {'var1': 1, 'var2': 2}}, 'parent_var': 1}
 
-        loaded_class = self.ins._parse_result_to_object(json, ClassWithTwoSubclasses, {'child_class': ClassWithSubclass}, {'sub_class': SimpleClass})
+        loaded_class = self.ins._parse_dict_to_object(json, ClassWithTwoSubclasses, {'child_class': ClassWithSubclass},
+                                                      {'sub_class': SimpleClass})
         self.assertEqual(1, loaded_class.parent_var)
         self.assertIsInstance(loaded_class.child_class, ClassWithSubclass)
         self.assertIsInstance(loaded_class.child_class.sub_class, SimpleClass)
@@ -81,7 +82,7 @@ class MongoClientParserTest(unittest.TestCase):
 
     def test_object_with_list_to_dict(self):
         json = {'my_list': [15, [42, 42, 42], 15], 'parent_var': 1}
-        loaded_class = self.ins._parse_result_to_object(json, ClassWith2DList)
+        loaded_class = self.ins._parse_dict_to_object(json, ClassWith2DList)
 
         # self.assertIsInstance(loaded_class, ClassWith2DList)
         self.assertIsInstance(loaded_class.my_list, list)
@@ -95,30 +96,32 @@ class MongoClientParserTest(unittest.TestCase):
     def test_dict_with_list_of_dict_to_object(self):
         json = {'my_list': [{'SimpleClass': {'var1': 1, 'var2': 2}}, {'SimpleClass': {'var1': 1, 'var2': 2}}], 'parent_var': 1}
 
-        loaded_class = self.ins._parse_result_to_object(json, ClassWithObjectsInList, {'SimpleClass': SimpleClass})
-
-        # self.assertIsInstance(loaded_class, ClassWithObjectsInList)
+        loaded_class = self.ins._parse_dict_to_object(json, ClassWithObjectsInList, {'SimpleClass': SimpleClass})
 
         self.assertEqual(1, loaded_class.parent_var)
         self.assertEqual(2, len(loaded_class.my_list))
 
+        self.assertIsInstance(loaded_class.my_list, list)
         self.assertIsInstance(loaded_class.my_list[0], SimpleClass)
         self.assertIsInstance(loaded_class.my_list[1], SimpleClass)
         self.assertEqual(1, loaded_class.my_list[0].var1)
         self.assertEqual(2, loaded_class.my_list[0].var2)
 
+    def test_json_with_2dlist_of_json_to_object(self):
+        json = {'parent_var': 1, 'my_list': [{'SimpleClass': {'var1': 1, 'var2': 2}}, {'SimpleClass': [{'SimpleClass': {'var1': 1, 'var2': 2}}]}]}
 
+        loaded_class = self.ins._parse_dict_to_object(json, ClassWithObjectsIn2DList, {'SimpleClass': SimpleClass})
+
+        self.assertEqual(1, loaded_class.parent_var)
+
+        self.assertIsInstance(loaded_class.my_list, list)
+        self.assertIsInstance(loaded_class.my_list[1], list)
+        self.assertIsInstance(loaded_class.my_list[0], SimpleClass)
+        self.assertIsInstance(loaded_class.my_list[1][0], SimpleClass)
+
+        self.assertEqual(1, loaded_class.my_list[0].var1)
+        self.assertEqual(1, loaded_class.my_list[1][0].var1)
     # endregion
-
-    # def test_object_with_list_of_object_to_dict(self):
-    #     cls = ClassWithObjectsInList()
-    #
-    #     self.assertEqual({'my_list': [{'SimpleClass': {'var1': 1, 'var2': 2}}, {'SimpleClass': {'var1': 1, 'var2': 2}}], 'parent_var': 1}, self.ins._parse_object_to_dict(cls))
-    #
-    # def test_object_with_2dlist_of_object_to_dict(self):
-    #     cls = ClassWithObjectsIn2DList()
-    #
-    #     self.assertEqual({'parent_var': 1, 'my_list': [{'SimpleClass': {'var1': 1, 'var2': 2}}, {'SimpleClass': [{'SimpleClass': {'var1': 1, 'var2': 2}}]}]},  self.ins._parse_object_to_dict(cls))
 
 
 if __name__ == '__main__':
