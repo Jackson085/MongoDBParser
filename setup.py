67db8daeb43d4689a9ef8610b2bdd51653323a76
Jackson085 <jackson26594916@gmail.com>
2023-10-13 19:43:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 import setuptools
 
-with open("README.md", "r", encoding = "utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "DatabaseConnector",
-    version = "0.1.0",
-    author = "Jackson085",
-    author_email = " ",
-    description = "Simple Parser for MongoDB",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "package URL",
-    project_urls = {
+    name="DatabaseConnector",
+    version="0.1.0",
+    author="Jackson085",
+    author_email=" ",
+    description="Simple Parser for MongoDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="package URL",
+    project_urls={
         "Bug Tracker": "https://github.com/Jackson085/DatabaseConnector/issues",
     },
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir = {"": "src"},
-    packages = setuptools.find_packages(where="src"),
-    python_requires = ">=3.9"
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9"
 )
